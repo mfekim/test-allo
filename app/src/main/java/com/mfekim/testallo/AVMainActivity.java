6789ad507c0dd383c60971e2c04fc61cb6a519eb
Mehdi Feki <mehdi.feki@palpix.com>
2017-12-16 11:04:04 +0100

    Create and init the project

---
+++ app/src/main/java/com/mfekim/testallo/AVMainActivity.java
@@ -0,0 +1,16 @@
+package com.mfekim.testallo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ */
+public class AVMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.av_activity_main);
+    }
+}
